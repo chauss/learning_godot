d4fd28893ee578c289ec7555cb43093529e1fe86
Christopher Hauß <christopher_hauss@gmx.de>
2024-02-07 20:13:02 +0100

    give some color to arrow amount label

--- scenes/user_interface/ui.gd
+++ scenes/user_interface/ui.gd
@@ -1,5 +1,8 @@
 extends CanvasLayer
 
+const green: Color = Color("14bc14")
+const red: Color = Color("fa352d")
+
 @onready var arrow_label = $MarginContainer/MarginContainer/ArrowLabel
 
 func _ready():
@@ -8,3 +11,7 @@ func _ready():
 	
 func update_arrow_label():
 	arrow_label.text = "Arrows: " + str(GameMaster.arrow_amount)
+	if GameMaster.arrow_amount > 0:
+		arrow_label.modulate = green
+	else:
+		arrow_label.modulate = red
